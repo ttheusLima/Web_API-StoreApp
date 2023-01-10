97c076537fb52532f9cdbbe2d1e481c5bf0902da
ttheusLima <ttheus.rlima@gmail.com>
2023-01-10 17:24:27 -0300

    Criado EndPoints de obter por Id e obter todos

---
+++ Api/StoreApp/Controllers/ProdutoController.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StoreApp.Entities;
+
+namespace StoreApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProdutoController : ControllerBase
+    {
+        List<Produto> _produtos = new List<Produto>();
+
+        public ProdutoController() { }
+        public ProdutoController(List<Produto> produtos)
+        {
+            _produtos = produtos;
+        }
+
+        public List<Produto> Get_produtos()
+        {
+            return _produtos;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Produto>> GetTodosProdutosAsync(List<Produto> _produtos)
+        {
+            return await Task.FromResult(Get_produtos());
+        }
+
+        public IActionResult GetProdutoId(int id)
+        {
+            var produto =  _produtos.FirstOrDefault(p => p.IdProduto == id);
+
+            return produto is null ? NotFound() : Ok(produto);
+        }
+
+        public async Task<IActionResult> GetProdutoId_Async(int id)
+        {
+            return await Task.FromResult(GetProdutoId(id));
+        }
+    }
+}
